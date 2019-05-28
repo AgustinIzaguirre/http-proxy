2e4d0f6edefde4fc4520856abb06292f94b2c049
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-05-28 14:26:38 -0300

    code separated and interests on fds updated correctly

--- proxy/handleRequest.c
+++ proxy/handleRequest.c
@@ -6,7 +6,6 @@
 #include <selector.h>
 
 unsigned requestRead(struct selector_key *key) {
-	httpADT_t state	= GET_DATA(key);
 	buffer *readBuffer = getReadBuffer(GET_DATA(key));
 	unsigned ret	   = HANDLE_REQUEST;
 	uint8_t *pointer;
@@ -16,33 +15,16 @@ unsigned requestRead(struct selector_key *key) {
 	// if there is no space to read should write what i already read
 	if (!buffer_can_write(readBuffer)) {
 		// set interest no op on fd an write on origin fd
-		if (SELECTOR_SUCCESS !=
-				selector_set_interest(key->s, key->fd, OP_NOOP) ||
-			SELECTOR_SUCCESS !=
-				selector_set_interest(key->s, getOriginFd(state), OP_WRITE)) {
-			return ERROR;
-		}
-
-		return ret;
+		return setAdecuateFdInterests(key);
 	}
 
 	pointer   = buffer_write_ptr(readBuffer, &count);
 	bytesRead = recv(key->fd, pointer, count, 0);
 
 	if (bytesRead > 0) {
-		// check if request finished TODO
 		buffer_write_adv(readBuffer, bytesRead);
-		if (!buffer_can_write(readBuffer)) {
-			// register no op on fd and write on origin fd
-			if (SELECTOR_SUCCESS !=
-					selector_set_interest(key->s, key->fd, OP_NOOP) ||
-				SELECTOR_SUCCESS != selector_set_interest(
-										key->s, getOriginFd(state), OP_WRITE)) {
-				return ERROR;
-			}
-
-			return ret;
-		}
+		// check if request finished TODO
+		ret = setAdecuateFdInterests(key);
 	}
 	else {
 		ret = ERROR;
@@ -52,7 +34,6 @@ unsigned requestRead(struct selector_key *key) {
 }
 
 unsigned requestWrite(struct selector_key *key) {
-	httpADT_t state	= GET_DATA(key);
 	buffer *readBuffer = getReadBuffer(GET_DATA(key));
 	unsigned ret	   = HANDLE_REQUEST;
 	uint8_t *pointer;
@@ -62,36 +43,44 @@ unsigned requestWrite(struct selector_key *key) {
 	// if everything is read on buffer
 	if (!buffer_can_read(readBuffer)) {
 		// set interest no op on fd an read on client fd
-		if (SELECTOR_SUCCESS !=
-				selector_set_interest(key->s, key->fd, OP_NOOP) ||
-			SELECTOR_SUCCESS !=
-				selector_set_interest(key->s, getClientFd(state), OP_READ)) {
-			return ERROR;
-		}
-
-		return ret;
+		return setAdecuateFdInterests(key);
 	}
 
 	pointer   = buffer_read_ptr(readBuffer, &count);
 	bytesRead = send(key->fd, pointer, count, 0);
 
 	if (bytesRead > 0) {
-		// check if request finished TODO
 		buffer_read_adv(readBuffer, bytesRead);
-		if (!buffer_can_read(readBuffer)) {
-			// register no op on fd and read on client fd
-			if (SELECTOR_SUCCESS !=
-					selector_set_interest(key->s, key->fd, OP_NOOP) ||
-				SELECTOR_SUCCESS != selector_set_interest(
-										key->s, getClientFd(state), OP_READ)) {
-				return ERROR;
-			}
-
-			return ret;
-		}
+		// check if request finished TODO
+		ret = setAdecuateFdInterests(key);
 	}
 	else {
 		ret = ERROR;
+	}
+
+	return ret;
+}
+
+unsigned setAdecuateFdInterests(struct selector_key *key) {
+	httpADT_t state	= GET_DATA(key);
+	buffer *readBuffer = getReadBuffer(GET_DATA(key));
+	unsigned ret	   = HANDLE_REQUEST;
+	int clientInterest = OP_NOOP;
+	int originInterest = OP_NOOP;
+
+	if (buffer_can_write(readBuffer)) {
+		clientInterest |= OP_READ;
+	}
+
+	if (buffer_can_read(readBuffer)) {
+		originInterest |= OP_WRITE;
+	}
+
+	if (SELECTOR_SUCCESS !=
+			selector_set_interest(key->s, getClientFd(state), clientInterest) ||
+		SELECTOR_SUCCESS !=
+			selector_set_interest(key->s, getOriginFd(state), originInterest)) {
+		return ERROR;
 	}
 
 	return ret;
--- proxy/include/handleRequest.h
+++ proxy/include/handleRequest.h
@@ -2,9 +2,19 @@
 #define HANDLE_REQUEST_H
 
 #include <selector.h>
-
+/*
+ * Reads request from client fd into readBuffer
+ */
 unsigned requestRead(struct selector_key *key);
 
+/*
+ * Writes from readBuffer to originFd
+ */
 unsigned requestWrite(struct selector_key *key);
+
+/*
+ * Set corresponding interests to client fd and origin fd
+ */
+unsigned setAdecuateFdInterests(struct selector_key *key);
 
 #endif
