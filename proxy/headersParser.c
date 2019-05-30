530da7646e2e260c294e86a0fe5b37bf743e5c6c
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-05-30 10:14:02 -0300

    headersParser added

---
+++ proxy/headersParser.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <headersParser.h>
+
+void parseHeaders(struct headersParser *header, buffer *input, int begining,
+				  int end) {
+	while (begining <= end) {
+		parseHeadersByChar(input->data[begining], header);
+		begining++;
+
+		if (header->state == HEADER_DONE) {
+			printf("%s\n", header->currHeader);
+			resetHeaderParser(header);
+		}
+		else if (header->state == BODY_START) {
+			printf("body start\n");
+		}
+	}
+}
+
+struct headersParser *initializeHeaderParser() {
+	struct headersParser *header = malloc(sizeof(struct headersParser));
+	// validate malloc
+	header->state		= HEADERS_START;
+	header->headerIndex = 0;
+	return header;
+}
+
+void parseHeadersByChar(char l, struct headersParser *header) {
+	int state = header->state;
+	switch (state) {
+		case HEADERS_START:
+			if (l == '\n') {
+				header->state = BODY_START;
+			}
+			else if (l == '\r') {
+				header->state = HEADER_END;
+			}
+			else if (l != ':') {
+				header->currHeader[header->headerIndex++] = l;
+				header->state							  = HEADER_NAME;
+			}
+			else {
+				header->state = HEADER_VALUE;
+			}
+
+			break;
+
+		case HEADER_NAME:
+			if (l == ':') {
+				header->currHeader[header->headerIndex++] = 0;
+				header->state							  = HEADER_VALUE;
+			}
+			else {
+				header->currHeader[header->headerIndex++] = l;
+			}
+
+			break;
+
+		case HEADER_VALUE:
+			if (l == '\n') {
+				header->state = HEADER_DONE;
+			}
+
+			break;
+
+		case HEADER_END:
+			if (l == '\n') {
+				header->state = BODY_START;
+			}
+			else if (l != ':') {
+				header->currHeader[header->headerIndex++] = l;
+				header->state							  = HEADER_NAME;
+			}
+			else {
+				header->state = HEADER_VALUE;
+			}
+
+			break;
+
+		case HEADER_DONE:
+		case BODY_START:
+
+			break;
+	}
+}
+
+void resetHeaderParser(struct headersParser *header) {
+	header->headerIndex = 0;
+	header->state		= HEADERS_START;
+}
---
+++ proxy/include/headersParser.h
@@ -0,0 +1,46 @@
+#ifndef HEADERS_PARSER_H
+#define HEADERS_PARSER_H
+
+#include <selector.h>
+#include <stdint.h>
+#include <buffer.h>
+
+#define MAX_HEADER_LENGTH 128
+
+struct headersParser {
+	char currHeader[MAX_HEADER_LENGTH];
+	int headerIndex;
+	int state;
+};
+
+enum headersState {
+	HEADERS_START,
+	HEADER_NAME,
+	HEADER_VALUE,
+	HEADER_END,
+	HEADER_DONE,
+	BODY_START,
+};
+
+/*
+ * Returns a new headers parser structure
+ */
+struct headersParser *initializeHeaderParser();
+
+/*
+ * Parse a char into the headers parser statemachine
+ */
+void parseHeadersByChar(char l, struct headersParser *header);
+
+/*
+ * Resets header parser structure to initial state and reset its index
+ */
+void resetHeaderParser(struct headersParser *header);
+
+/*
+ * Parse the chars in buffer from begining to end inclusive
+ */
+void parseHeaders(struct headersParser *header, buffer *input, int begining,
+				  int end);
+
+#endif
