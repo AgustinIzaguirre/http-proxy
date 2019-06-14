2e4acddc368cf5a97586223545ec9bb6857d9d3a
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2019-06-13 22:45:21 -0300

    More refactoring

--- proxy/include/logger.h
+++ proxy/include/logger.h
@@ -2,17 +2,25 @@
 #define LOGGER_H
 
 #include <netinet/in.h>
+
+#define FAILED 0
+#define OK 1
+
 /* Create a new log file */
 void createLogFile();
 
 /* Add a new entry (line) to an existing log file */
-int addLogEntry();
-
-/* Create a new log entry and save it in a buffer */
-int createLogEntryStr(struct sockaddr_storage *clientAddr,
-	char *logEntryBuffer);
+int addEntryToLog(struct sockaddr_storage *clientAddr, const char *logFileName);
 
 int getIpAddresses(struct sockaddr_storage *clientAddr, char *host,
 	char *server);
+
+/* Setters for the access log array of elements to construct an entry */
+void setRemoteAddr(char **accessLog, const char *remoteAddr);
+void setTime(char **accessLog);
+void setRequest(char **accessLog, const char *request);
+void setStatus(char **accessLog, const char *status);
+void setHttpReferer(char **accessLog, const char *httpReferer);
+void setHttpUserAgent(char **accessLog, const char *httpUserAgent);
 
 #endif
--- proxy/logger.c
+++ proxy/logger.c
@@ -1,52 +1,101 @@
 #include <logger.h>
 #include <stdio.h>
-/* TODO: Check the reason for gcc showin a warning telling me to include
+/* TODO: Check the reason for gcc showing a warning telling me to include
  * <stdio.h> for the usage of snprintf when I have already included the library
  */
 #include <time.h>
 #include <http.h>
 #include <httpProxyADT.h>
 
-void createLogFile(const char *path) {
-  // TODO: Check if size makes sense & if it should be set as #define
-  size_t size = 64;
-  char newLogFilePath[size];
-  char *accessLog = "access.log";
-  int bytesSupposedToWrite = 0;
+enum accessLogElems {
+  REMOTE_ADDR,
+  TIME,
+  REQUEST,
+  STATUS,
+  // TODO: BODY_BYTES_SENT,
+  HTTP_REFERER,
+  HTTP_USER_AGENT
+};
 
-  if (path == NULL)
-    strcpy(newLogFilePath, "./");
-  else
-    strcpy(newLogFilePath, path);
+char *createPath(char *newLogFilePath, char *path) {
+  if (path == NULL) {
+    path = "./";
+  }
 
-  bytesSupposedToWrite = snprintf(newLogFilePath + size, sizeof newLogFilePath,
-      "%s%s", newLogFilePath, accessLog);
+  bytesWritten = snprintf(newLogFilePath, sizeof newLogFilePath, "%s", path);
 
-  fopen(newLogFilePath, "+a");
+  if (bytesWritten >= sizeof newLogFilePath) {
+    return NULL;
+  }
+
+  bytesWritten = snprintf(newLogFilePath + sizeof(path),
+    sizeof(newLogFilePath) - sizeof(path), "access.log");
+
+  if (bytesWritten >= (sizeof(newLogFilePath) - sizeof(path))) {
+    return NULL;
+  }
+
+  return newLogFilePath;
 }
 
-int addLogEntry(struct sockaddr_storage *clientAddr, const char *logFileName) {
+int createLogFile(char *path) {
+  // TODO: Check if size makes sense & if it should be set as #define
+  size_t size = 64;
+  char *newLogFilePath = malloc(size);
+  char *accessLog = "access.log";
+  int bytesWritten = 0;
+
+  newLogFilePath = createPath(newLogFilePath, path);
+
+  if (newLogFilePath == NULL) {
+    return FAILED;
+  }
+
+  return fopen(newLogFilePath, "+a") == NULL ? FAILED : OK;
+}
+
+char *createLogEntry(const char **logElems, size_t logElemsSize, char *buff,
+  size_t buffSize) {
+  char const *end = buff + buffSize;
+  int bytesWritten = 0;
+
+  // TODO: Format correctly
+  for (int i = 0; (i < logElemsSize) && (buff < end); i++) {
+    bytesWritten = snprintf(buff, end - buff, "%s", logElems[i]);
+
+    if (bytesWritten >= (end - buff))
+      return FAILED;
+    else
+      buff += bytesWritten;
+  }
+
+  return buff < end ? buff : NULL;
+}
+
+int addEntryToLog(struct sockaddr_storage *clientAddr,
+  const char *logFileName) {
   // TODO: Check if size makes sense & if it should be set as #define's
   size_t maxLogEntrySize = 512;
   char logEntryBuffer[maxLogEntrySize];
   FILE *logFile;
-  int ret = 0;
+  int couldWriteToFile = 0;
 
-  if (!createLogEntryStr(clientAddr, logEntryBuffer))
-    return ERROR;
+  if (!createLogEntry(clientAddr, logEntryBuffer)) {
+    return FAILED;
+  }
 
   logFile = fopen (logFileName, "a+");
 
-  ret = fprintf(logFile, "%s", logEntryBuffer) <= 0;
+  couldWriteToFile = (fprintf(logFile, "%s", logEntryBuffer) >= 0);
   fclose(logFile);
 
-  return ret;
+  return couldWriteToFile;
 }
 
+/*
 // TODO: Receive the response status saved in the proxy
 int createLogEntryStr(struct sockaddr_storage *clientAddr,
     char *logEntryBuffer) {
-  // TODO: Check if sizes make sense & if they should be set as #define's
   size_t maxAddrSize = 512;
   size_t maxRequestSize = 1024;
   char host[maxAddrSize], server[maxAddrSize];
@@ -56,15 +105,13 @@ int createLogEntryStr(struct sockaddr_storage *clientAddr,
   int couldGetIpAddresses = getIpAddresses(clientAddr, host, server);
   int ret = 0;
 
-  if (couldGetIpAddresses != 0)
-    return ERROR;
-
-  bytesSupposedToWrite = snprintf(logEntryBuffer, sizeof(logEntryBuffer), "%s",
-    host);
-  ret = bytesSupposedToWrite < sizeof(logEntryBuffer);
+  if (couldGetIpAddresses != 0) {
+    return FAILED;
+  }
 
   return ret;
 }
+*/
 
 int getIpAddresses(struct sockaddr_storage *clientAddr, char *host,
     char *server) {
@@ -72,7 +119,34 @@ int getIpAddresses(struct sockaddr_storage *clientAddr, char *host,
     sizeof(host), server, sizeof(server), NI_NAMEREQD);
 }
 
-char *getTime() {
+char **createLogElemsArray() {
+  size_t logElemsAmount = 7; // TODO: Should be 8 with BODY_BYTES_SENT
+  char **logElems = malloc(logElemsAmount);
+
+  return logElems;
+}
+
+void setRemoteAddr(char **accessLog, const char *remoteAddr) {
+  accessLog[REMOTE_ADDR] = remoteAddr;
+}
+
+void setTime(char **accessLog) {
   time_t now = time(0);
-  return ctime(&now);
+  accessLog[TIME] = ctime(&now);
+}
+
+void setRequest(char **accessLog, const char *request) {
+  accessLog[REQUEST] = request;
+}
+
+void setStatus(char **accessLog, const char *status) {
+  accessLog[STATUS] = status;
+}
+
+void setHttpReferer(char **accessLog, const char *httpReferer) {
+  accessLog[HTTP_REFERER] = httpReferer;
+}
+
+void setHttpUserAgent(char **accessLog, const char *httpUserAgent) {
+  accessLog[HTTP_USER_AGENT] = httpUserAgent;
 }
